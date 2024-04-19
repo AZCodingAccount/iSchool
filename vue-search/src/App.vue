d6e4833132d9101b28daec7d6e0cbd37dc5464fd
LJX <1476652531@qq.com>
2024-04-19 19:44:22 +0800

    搭建框架

---
+++ vue-search/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style scoped>
+#app {
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+}
+</style>
---
+++ vue-search/src/main.js
@@ -0,0 +1,18 @@
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import persist from 'pinia-plugin-persistedstate'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+app.use(createPinia().use(persist))
+app.use(router)
+app.use(ElementPlus)
+app.mount('#app')
