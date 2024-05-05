9ae70fcec2caaee1be97d4958925372aaaf6ed65
AlbertZhang <han892577@163.com>
2024-05-05 19:41:01 +0800

    demo: 添加spring ai使用demo

---
+++ iSchool-Server/iSchool-service/ischool-search/src/test/java/com/search/IschoolCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IschoolCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
