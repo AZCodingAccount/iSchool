4730460ae1c916cde77c44ecd3a5dcb9bcb010c1
AlbertZhang <han892577@163.com>
2024-05-05 22:06:27 +0800

    feat: 集成rabbitmq消息队列异步处理ai请求

--- iSchool-Server/iSchool-service/ischool-search/src/main/java/com/search/controller/SearchController.java
+++ iSchool-Server/iSchool-service/ischool-search/src/main/java/com/search/controller/SearchController.java
@@ -1,9 +1,12 @@
 package com.search.controller;
 
+import cn.hutool.core.map.MapUtil;
+import cn.hutool.core.util.IdUtil;
 import com.ischool.model.BaseResponse;
 import com.ischool.model.PageResult;
 import com.ischool.model.Result;
 import com.search.model.vo.SearchAnnouncementVO;
+import com.search.mq.AiMessageProducer;
 import com.search.service.InfoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @program: iSchool-Server
@@ -28,6 +33,9 @@ public class SearchController {
     @Autowired
     InfoService infoService;
 
+    @Autowired
+    AiMessageProducer aiMessageProducer;
+
     /**
      * @param keyword
      * @param pageNum
@@ -41,8 +49,24 @@ public class SearchController {
                                                                              @RequestParam("pageSize") Integer pageSize) {
         log.info("用户搜索，搜索信息为：{}，分页查询：{}，{}", keyword, pageNum, pageSize);
         PageResult<SearchAnnouncementVO> pageResult = infoService.search(keyword, pageNum, pageSize);
-        // todo：消息队列检索
         return Result.success(pageResult);
+    }
+
+    /**
+     * @param keyword
+     * @return com.ischool.model.BaseResponse<java.lang.Object>
+     * @description 生成ai评论
+     **/
+    @GetMapping("/ai")
+    public BaseResponse<Object> aiSearch(@RequestParam("keyword") String keyword) {
+        log.info("用户搜索，搜索信息为：{}", keyword);
+        // 使用雪花算法生成一个id，用于追踪这个搜索信息
+        String id = IdUtil.getSnowflakeNextIdStr();
+        Map<String, Object> message = new HashMap<>();
+        message.put("id", id);
+        message.put("message", keyword);
+        aiMessageProducer.sendMessage(message);
+        return Result.success();
     }
 
 
