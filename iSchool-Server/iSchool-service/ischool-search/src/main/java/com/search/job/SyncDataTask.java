1d2e711218755f9404696f01f6200de51ebf9187
AlbertZhang <han892577@163.com>
2024-05-16 21:55:02 +0800

    docs: 更新文档

---
+++ iSchool-Server/iSchool-service/ischool-search/src/main/java/com/search/job/SyncDataTask.java
@@ -0,0 +1,11 @@
+package com.search.job;/**
+* @program: iSchool-Server
+*
+* @author: AlbertZhang
+*
+* @create: 2024-05-16 21:49
+*
+* @description: 同步数据定时任务
+**/
+public class SyncDataTask {
+}
---
+++ iSchool-Server/iSchool-service/ischool-search/src/main/java/com/search/job/XxlDemo.java
@@ -0,0 +1,11 @@
+package com.search.job;/**
+* @program: iSchool-Server
+*
+* @author: AlbertZhang
+*
+* @create: 2024-05-16 20:41
+*
+* @description: xxl测试类
+**/
+public class XxlDemo {
+}
