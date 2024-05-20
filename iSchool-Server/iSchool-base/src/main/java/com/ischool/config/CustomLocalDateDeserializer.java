d8c942f0111d12db5ad5ef37a47185ba825f16e1
AlbertZhang <han892577@163.com>
2024-05-20 13:38:24 +0800

    docs: 总结优化思路

---
+++ iSchool-Server/iSchool-base/src/main/java/com/ischool/config/CustomLocalDateDeserializer.java
@@ -0,0 +1,11 @@
+package com.ischool.config;/**
+* @program: iSchool-Server
+*
+* @author: AlbertZhang
+*
+* @create: 2024-05-19 22:17
+*
+* @description: 
+**/
+public class CustomLocalDateDeserializer {
+}
---
+++ iSchool-Server/iSchool-service/ischool-search/src/main/java/com/search/es/SyncESService.java
@@ -0,0 +1,11 @@
+package com.search.es;/**
+* @program: iSchool-Server
+*
+* @author: AlbertZhang
+*
+* @create: 2024-05-19 17:25
+*
+* @description: ES数据同步实现类
+**/
+public class SyncESService {
+}
